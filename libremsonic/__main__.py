1df026d9eebad648c9574cb8ee2d8bb9c7719a31
Sumner Evans <sumner.evans98@gmail.com>
2019-06-22 11:25:25 -0600

    Removed server test code

--- libremsonic/__main__.py
+++ libremsonic/__main__.py
@@ -5,18 +5,9 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-import mpv
-
 from .app import LibremsonicApp
-from .server import Server
 
 
 def main():
-    server = Server('ohea', 'https://airsonic.the-evans.family', 'sumner',
-                    'O}/UieSb[nzZ~l[X1S&zzX1Hi')
-
-    print(server.ping())
-    # print(server.search2('The Band Perry'))
-    stream = server.download(740)
-    # app = LibremsonicApp()
-    # app.run(sys.argv)
+    app = LibremsonicApp()
+    app.run(sys.argv)
