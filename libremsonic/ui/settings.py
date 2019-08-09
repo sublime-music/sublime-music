e9838974dbaf4890e07bb82dbced686152631c10
Sumner Evans <sumner.evans98@gmail.com>
2019-08-08 22:50:33 -0600

    Actually added the settings editor file

---
+++ libremsonic/ui/settings.py
@@ -0,0 +1,33 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject
+
+from .common.edit_form_dialog import EditFormDialog
+
+
+class SettingsDialog(EditFormDialog):
+    title: str = 'Settings'
+    initial_size = (450, 250)
+    text_fields = []
+    boolean_fields = [
+        ('Show headers on song lists', 'show_headers'),
+        ('Always stream songs', 'always_stream'),
+        ('When streaming, also download song', 'download_on_stream'),
+    ]
+    numeric_fields = [
+        (
+            'How many songs in the play queue do you want to prefetch?',
+            'prefetch_amount',
+            (0, 10, 1),
+            0,
+        ),
+        (
+            'How many song downloads do you want to allow concurrently?',
+            'concurrent_download_limit',
+            (1, 10, 1),
+            5,
+        ),
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
