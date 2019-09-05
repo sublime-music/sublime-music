1903c46c65ab72bdbb70cbd34e3e464c0ff09075
Robby Zampino <robozman@gmail.com>
2019-09-05 15:04:36 -0600

    Updated CICD to export flatpak to file and make it available as a build artifact.

--- flatpak/flatpak_build.sh
+++ flatpak/flatpak_build.sh
@@ -5,4 +5,8 @@ sed -i "s/filename.endswith('gz')/filename.endswith('gz') or filename.endswith('
 
 python3 ./flatpak-builder-tools/pip/flatpak-pip-generator --requirements-file=flatpak-requirements.txt --output pypi-dependencies
 
-flatpak-builder flatpak_build_dir com.sumnerevans.libremsonic.json
+mkdir /repo
+
+flatpak-builder --repo=/repo flatpak_build_dir com.sumnerevans.libremsonic.json
+
+flatpak build-bundle /repo libremsonic.flatpak com.sumnerevans.libremsonic
