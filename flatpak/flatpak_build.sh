e3bce8c589b402f930223249f27982d193c6ff3b
Robby Zampino <robozman@gmail.com>
2019-09-04 18:33:07 -0600

    Added flatpak build script

---
+++ flatpak/flatpak_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/flatpak/flatpak-builder-tools.git
+sed -i "s/filename.endswith('gz')/filename.endswith('gz') or filename.endswith('zip')/g" flatpak-builder-tools/pip/flatpak-pip-generator
+
+python3 ./flatpak-builder-tools/pip/flatpak-pip-generator --requirements-file=flatpak-requirements.txt --output pypi-dependencies
+
+flatpak-builder flatpak_build_dir com.sumnerevans.libremsonic.json
