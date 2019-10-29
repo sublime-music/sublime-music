4ae2e5b0e5d081135f28f5432bea9fe72b55c3c4
Sumner Evans <sumner.evans98@gmail.com>
2019-10-29 13:04:37 -0600

    Fixed Flatpak build URL in release notes

--- cicd/publish-gitlab-release.sh
+++ cicd/publish-gitlab-release.sh
@@ -38,7 +38,7 @@ data="
         \"links\": [
             {
                 \"name\": \"sublime-music-${CI_COMMIT_TAG}.flatpak\",
-                \"url\": \"${CI_PROJECT_URL}/-/jobs/artifacts/${CI_COMMIT_REF_SLUG}/raw/sublime-music.flatpak?job=build_flatpak\"
+                \"url\": \"${CI_PROJECT_URL}/-/jobs/artifacts/${CI_COMMIT_REF_SLUG}/raw/flatpak/sublime-music.flatpak?job=build_flatpak\"
             }
         ]
     }
