5e819fa3b45735611dbb853bcb61a6b3c9c6729e
Sumner Evans <sumner.evans98@gmail.com>
2019-10-29 09:47:47 -0600

    Add GitLab Release script

---
+++ cicd/publish-gitlab-release.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+data="
+{
+    \"name\": \"${CI_COMMIT_TAG}\",
+    \"tag_name\": \"${CI_COMMIT_TAG}\",
+    \"description\": \"Test release from the GitLab CI/CD\",
+    \"milestones\": []
+}
+"
+
+echo $data
+
+cat CHANGELOG
+
+curl \
+    --header 'Content-Type: application/json' \
+    --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" \
+    ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/releases
