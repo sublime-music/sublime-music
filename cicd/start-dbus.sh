be16c4578e14b45dbb7bacaa3a16fbb47d612ced
Sumner Evans <sumner.evans98@gmail.com>
2020-01-15 09:14:29 -0700

    Add comment to start-dbus script

--- cicd/start-dbus.sh
+++ cicd/start-dbus.sh
@@ -1,3 +1,6 @@
+#! /bin/sh
+# Technique from https://georgik.rocks/how-to-start-d-bus-in-docker-container/
+
 dbus-uuidgen > /var/lib/dbus/machine-id
 mkdir -p /var/run/dbus
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
