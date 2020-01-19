33542429c54f24d7a1319f299d5579963ea63d4f
Sumner Evans <sumner.evans98@gmail.com>
2020-01-18 18:51:32 -0700

    Made flatpak_build.sh more idempotent

--- flatpak/flatpak_build.sh
+++ flatpak/flatpak_build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -xe
+
+rm -rf flatpak-builder-tools
 git clone https://github.com/flatpak/flatpak-builder-tools.git
 
 python3 ./flatpak-builder-tools/pip/flatpak-pip-generator --requirements-file=flatpak-requirements.txt --output pypi-dependencies
 
-mkdir /repo
+mkdir -p /repo
 
-flatpak-builder --repo=/repo flatpak_build_dir com.sumnerevans.SublimeMusic.json
+flatpak-builder --force-clean --repo=/repo flatpak_build_dir com.sumnerevans.SublimeMusic.json
 
 flatpak build-bundle /repo sublime-music.flatpak com.sumnerevans.SublimeMusic
