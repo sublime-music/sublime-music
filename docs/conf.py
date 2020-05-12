116571d9f682ec09f3aca64d77cd74a0d20a3c71
Louis-Philippe Véronneau <pollo@debian.org>
2020-05-11 23:25:29 -0400

    add a manpage for sublime-music

--- docs/conf.py
+++ docs/conf.py
@@ -82,3 +82,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for manual page output ---------------------------------------
+
+man_pages = [
+    ('manpages/sublime-music', 'sublime-music', u'native GTK *sonic client',
+     'Louis-Philippe Véronneau', 1)
+]
