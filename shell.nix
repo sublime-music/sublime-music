fba3c420ac11668ff1fb64f8d931d12716562d8e
Sumner Evans <me@sumnerevans.com>
2021-02-03 13:46:58 -0700

    Cleanup shell.nix and .envrc

--- shell.nix
+++ shell.nix
@@ -40,6 +40,8 @@ pkgs.mkShell {
     export LD_LIBRARY_PATH=${pkgs.mpv}/lib
     export XDG_DATA_DIRS="$GSETTINGS_SCHEMA_PATH:${pkgs.arc-theme}/share:${pkgs.arc-icon-theme}/share"
     export SOURCE_DATE_EPOCH=315532800
+
+    # An update happened to the shell.nix, so remove and reinstall everything in the virtualenv
     rm -rf .venv
     poetry install -E chromecast -E keyring -E server
     set +x
