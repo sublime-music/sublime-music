d2b02de65b674c3bf7853631dd414d1bbef91533
Sumner Evans <me@sumnerevans.com>
2023-06-02 11:17:02 -0600

    flake.nix: add sublime-music flake output
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -10,14 +10,70 @@
       (system:
         let
           pkgs = import nixpkgs-unstable { system = system; };
+          nativeBuildInputs = with pkgs; [
+            gobject-introspection
+            python3Packages.setuptools
+            wrapGAppsHook
+          ];
         in
         {
+          packages.sublime-music = pkgs.python3Packages.buildPythonApplication
+            rec {
+              pname = "sublime-music";
+              version = "0.12.0";
+              format = "flit";
+
+              src = ./.;
+
+              inherit nativeBuildInputs;
+
+              buildInputs = with pkgs; [
+                gtk3
+                pango
+                libnotify
+                networkmanager
+              ];
+
+              propagatedBuildInputs = with pkgs.python3Packages; [
+                bleach
+                bottle
+                dataclasses-json
+                deepdiff
+                keyring
+                mpv
+                peewee
+                PyChromecast
+                pygobject3
+                python-dateutil
+                python-Levenshtein
+                requests
+                semver
+                thefuzz
+              ];
+
+              # hook for gobject-introspection doesn't like strictDeps
+              # https://github.com/NixOS/nixpkgs/issues/56943
+              strictDeps = false;
+
+              # Skip checks
+              doCheck = false;
+
+              # Also run the python import check for sanity
+              pythonImportsCheck = [ "sublime_music" ];
+
+              postInstall = ''
+                install -Dm444 sublime-music.desktop      -t $out/share/applications
+                install -Dm444 sublime-music.metainfo.xml -t $out/share/metainfo
+
+                for size in 16 22 32 48 64 72 96 128 192 512 1024; do
+                    install -Dm444 logo/rendered/"$size".png \
+                      $out/share/icons/hicolor/"$size"x"$size"/apps/sublime-music.png
+                done
+              '';
+            };
+
           devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              gobject-introspection
-              python3Packages.setuptools
-              wrapGAppsHook
-            ];
+            inherit nativeBuildInputs;
 
             buildInputs = with pkgs; [
               bashInteractive
