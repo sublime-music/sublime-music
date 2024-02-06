c71dc8e8cf4eabb84f7ca91056b39ae6a151815f
Sumner Evans <me@sumnerevans.com>
2024-02-06 11:46:59 -0700

    subsonic/playlist: write client functions
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkg/subsonic/playlist.go
+++ pkg/subsonic/playlist.go
@@ -6,17 +6,30 @@ import (
 	"strconv"
 )
 
-func (c *Client) GetPlaylists(ctx context.Context) ([]Playlist, error) {
-	resp, err := c.getJSON(ctx, "/rest/getPlaylists.view", nil)
+// GetPlaylists returns all playlists a user is allowed to play.
+//
+// Docs: [Subsonic], [OpenSubsonic]
+//
+// [Subsonic]: http://www.subsonic.org/pages/api.jsp#getPlaylists
+// [OpenSubsonic]: https://opensubsonic.netlify.app/docs/endpoints/getplaylists/
+func (c *Client) GetPlaylists(ctx context.Context, username *string) (*Playlists, error) {
+	params := url.Values{}
+	if username != nil {
+		params.Set("username", *username)
+	}
+	resp, err := c.getJSON(ctx, "/rest/getPlaylists.view", params)
 	if err != nil {
 		return nil, err
-	} else if resp.SubsonicResponse.Playlists == nil {
-		return nil, nil
 	}
-
-	return resp.SubsonicResponse.Playlists.Playlist, nil
+	return resp.SubsonicResponse.Playlists, nil
 }
 
+// GetPlaylist returns a listing of files in a saved playlist.
+//
+// Docs: [Subsonic], [OpenSubsonic]
+//
+// [Subsonic]: http://www.subsonic.org/pages/api.jsp#getPlaylist
+// [OpenSubsonic]: https://opensubsonic.netlify.app/docs/endpoints/getplaylist/
 func (c *Client) GetPlaylist(ctx context.Context, playlistID SubsonicID) (*Playlist, error) {
 	resp, err := c.getJSON(ctx, "/rest/getPlaylist.view", url.Values{
 		"id": {playlistID.String()},
@@ -27,21 +40,18 @@ func (c *Client) GetPlaylist(ctx context.Context, playlistID SubsonicID) (*Playl
 	return resp.SubsonicResponse.Playlist, err
 }
 
+// ReqCreatePlaylist is the arguments to [Client.CreatePlaylist].
+type ReqCreatePlaylist struct {
+	PlaylistID SubsonicID   `url:"playlistId,omitempty"`
+	Name       *string      `url:"name,omitempty"` // Required if creating
+	SongIDs    []SubsonicID `url:"songId,omitempty"`
+}
+
+// CreatePlaylist creates a new playlist or updates an existing one.
 func (c *Client) CreatePlaylist(ctx context.Context, name string, songIDs []SubsonicID) (*Playlist, error) {
 	resp, err := c.getJSON(ctx, "/rest/createPlaylist.view", url.Values{
 		"name":   {name},
 		"songId": idsToStrings(songIDs),
-	})
-	if err != nil {
-		return nil, err
-	}
-	return resp.SubsonicResponse.Playlist, err
-}
-
-func (c *Client) ReorderPlaylistSongs(ctx context.Context, playlistID SubsonicID, newSongOrder []SubsonicID) (*Playlist, error) {
-	resp, err := c.getJSON(ctx, "/rest/createPlaylist.view", url.Values{
-		"playlistId": {playlistID.String()},
-		"songId":     idsToStrings(newSongOrder),
 	})
 	if err != nil {
 		return nil, err
