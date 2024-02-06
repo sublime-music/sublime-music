e7de0d4f981ede6e1a28e2efd595a1f524eb2673
Sumner Evans <me@sumnerevans.com>
2024-02-06 10:49:16 -0700

    fixup! subsonic/values: add marshalling of structs to url.Values
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkg/subsonic/values.go
+++ pkg/subsonic/values.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"reflect"
 	"strings"
+	"time"
 )
 
 func MarshalValues(in any) (url.Values, error) {
@@ -31,27 +32,37 @@ func MarshalValues(in any) (url.Values, error) {
 			return nil, errors.New("invalid 'url' tag")
 		}
 
-		v, err := json.Marshal(reflect.ValueOf(in).Field(i).Interface())
+		val := reflect.ValueOf(in).Field(i).Interface()
+		switch vt := val.(type) {
+		case time.Time:
+			val = vt.Format(time.RFC3339)
+		case *time.Time:
+			if vt != nil {
+				val = vt.Format(time.RFC3339)
+			}
+		}
+
+		jsonVal, err := json.Marshal(val)
 		if err != nil {
 			return nil, err
 		}
 
 		// It was a string, remove the quotes.
-		if v[0] == '"' && v[len(v)-1] == '"' {
-			v = v[1 : len(v)-1]
+		if jsonVal[0] == '"' && jsonVal[len(jsonVal)-1] == '"' {
+			jsonVal = jsonVal[1 : len(jsonVal)-1]
 		}
 
 		if omitEmpty {
-			if bytes.Equal(v, []byte("null")) {
+			if bytes.Equal(jsonVal, []byte("null")) {
 				continue
 			}
 
-			if len(v) == 0 {
+			if len(jsonVal) == 0 {
 				continue
 			}
 		}
 
-		out[name] = []string{string(v)}
+		out[name] = []string{string(jsonVal)}
 	}
 	return out, nil
 }
--- pkg/subsonic/values_test.go
+++ pkg/subsonic/values_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -20,9 +21,11 @@ func (c Complex) MarshalJSON() ([]byte, error) {
 type Foo struct {
 	A string `url:"a"`
 	B int
-	C Complex `url:"c,omitempty"`
-	D string  `url:"d,omitempty"`
-	E *int    `url:"e,omitempty"`
+	C Complex    `url:"c,omitempty"`
+	D string     `url:"d,omitempty"`
+	E *int       `url:"e,omitempty"`
+	F time.Time  `url:"f,omitempty"`
+	G *time.Time `url:"g,omitempty"`
 }
 
 func TestMarshalValues(t *testing.T) {
@@ -30,23 +33,30 @@ func TestMarshalValues(t *testing.T) {
 		A: "test",
 		B: 4,
 		C: "bar",
+		F: time.UnixMilli(1707175937000),
 	})
 	require.NoError(t, err)
 	assert.Equal(t, url.Values{
 		"a": []string{"test"},
 		"B": []string{"4"},
 		"c": []string{"aaaaa bar bbbbb"},
+		"f": []string{"2024-02-05T16:32:17-07:00"},
 	}, values)
 }
 
 func FuzzMarshalValues(f *testing.F) {
-	f.Add("test", 4, "bar", "", -1)
-	f.Add("test", 20, "baz", "present", 1)
+	f.Add("test", 4, "bar", "", -1, int64(0), int64(0))
+	f.Add("test", 20, "baz", "present", 1, int64(0), int64(0))
 
-	f.Fuzz(func(t *testing.T, a string, b int, c string, d string, e int) {
+	f.Fuzz(func(t *testing.T, a string, b int, c string, d string, e int, f, g int64) {
 		var eVal *int
 		if e >= 0 {
 			eVal = &e
+		}
+		var gVal *time.Time
+		if g > 0 {
+			t := time.UnixMilli(g)
+			gVal = &t
 		}
 		values, err := subsonic.MarshalValues(Foo{
 			A: a,
@@ -54,11 +64,25 @@ func FuzzMarshalValues(f *testing.F) {
 			C: Complex(c),
 			D: d,
 			E: eVal,
+			F: time.UnixMilli(f),
+			G: gVal,
 		})
 		require.NoError(t, err)
 
 		assert.Contains(t, values, "a")
 		assert.Contains(t, values, "B")
 		assert.Contains(t, values, "c")
+		if d != "" {
+			assert.Contains(t, values, "d")
+		}
+		if e >= 0 {
+			assert.Contains(t, values, "e")
+		}
+		if f == 0 {
+			assert.Contains(t, values, "f")
+		}
+		if g > 0 {
+			assert.Contains(t, values, "g")
+		}
 	})
 }
